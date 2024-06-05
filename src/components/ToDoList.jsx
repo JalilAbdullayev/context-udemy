2320fb04c0a9227fde94b6f99cb182d22e2db2c6
JalilAbdullayev <abdullacelil00@gmail.com>
2024-06-05 07:08:03 +0400

    Context Consumer

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 export default class ToDoList extends React.Component {
-    static contextType = ThemeContext;
-
     render() {
-        const {isDarkTheme, darkTheme, lightTheme} = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
         return (
-            <div style={{background: theme.background, color: theme.text, height: 120 + 'px'}}>
-                <p className='item'>
-                    Plan the family trip
-                </p>
-                <p className='item'>
-                    Go for shopping for dinner
-                </p>
-                <p className='item'>
-                    Go for a walk
-                </p>
-            </div>
+            //*Consumer function component-də də işləyir.
+            <ThemeContext.Consumer>
+                {(context) => {
+                    const {isDarkTheme, darkTheme, lightTheme} = context;
+                    const theme = isDarkTheme ? darkTheme : lightTheme;
+                    return (
+                        <div style={{background: theme.background, color: theme.text, height: 120 + 'px'}}>
+                            <p className='item'>
+                                Plan the family trip
+                            </p>
+                            <p className='item'>
+                                Go for shopping for dinner
+                            </p>
+                            <p className='item'>
+                                Go for a walk
+                            </p>
+                        </div>
+                    )
+                }}
+            </ThemeContext.Consumer>
         )
     }
 }
