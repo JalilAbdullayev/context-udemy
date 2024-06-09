2925638c120a21853a64c8a8b6a50143591e79df
JalilAbdullayev <abdullacelil00@gmail.com>
2024-06-09 19:05:50 +0400

    Multiple Contexts with useContext

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,32 @@
-import React from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
 
-export default class Navbar extends React.Component {
-    static contextType = ThemeContext;
-
-    render() {
-        const {isDarkTheme, darkTheme, lightTheme} = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
-        return (
-            <nav style={{background: theme.background, height: 120 + 'px'}}>
-                <h2 className='ui centered header' style={{color: theme.text}}>
-                    Title
-                </h2>
-                <div className="ui three buttons">
-                    <button className="ui button">
-                        Overview
-                    </button>
-                    <button className="ui button">
-                        Contact
-                    </button>
-                    <button className="ui button">
-                        Support
-                    </button>
-                </div>
-            </nav>
-        )
-    }
+const Navbar = () => {
+    const {isDarkTheme, darkTheme, lightTheme} = useContext(ThemeContext);
+    const {isLoggedIn, changeAuthStatus} = useContext(AuthContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    return (
+        <nav style={{background: theme.background, height: 120 + 'px'}}>
+            <h2 className='ui centered header' style={{color: theme.text}}>
+                Title
+            </h2>
+            <p onClick={changeAuthStatus} style={{color: theme.text}}>
+                {isLoggedIn ? 'Logged In' : 'Logged Out'}
+            </p>
+            <div className="ui three buttons">
+                <button className="ui button">
+                    Overview
+                </button>
+                <button className="ui button">
+                    Contact
+                </button>
+                <button className="ui button">
+                    Support
+                </button>
+            </div>
+        </nav>
+    )
 }
+
+export default Navbar;
